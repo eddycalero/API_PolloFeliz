06c6b4f82e42ab2ff52fbea9c2d6e476f7042092
Eddy <caleroeddy99@gmail.com>
2025-10-26 21:59:20 -0600

    Creando la configuracion con dapper e instalando los paquetes nugget

---
+++ UNI.Models/DbContext/UNIDapperContext.cs
@@ -0,0 +1,39 @@
+﻿
+
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System.Data;
+
+namespace UNI.Models
+{
+    public class UNIDapperContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string? _connectionString;
+
+        private IDbConnection _dbConnection;
+        private IDbTransaction _dbTransaction;
+        public UNIDapperContext( IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IDbConnection Connection {
+            get {
+                if(_dbConnection  is null || _dbConnection.State != ConnectionState.Open)
+                {
+                    _dbConnection = new NpgsqlConnection( _connectionString );
+                }
+                return _dbConnection;
+                    
+             }
+        }
+
+        public IDbTransaction Transaction
+        {
+            get => _dbTransaction;
+            set => _dbTransaction = value;
+        }
+    }
+}
---
+++ UNI.Models/Entity/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace UNI.Models
+{
+    [Table ("product.category")]
+    public class Category
+    {
+        
+        [Key]
+        public int category_id {  get; set; }
+        public string name  { get; set; } = string.Empty;
+
+        public bool is_active { get; set; }
+    }
+}
