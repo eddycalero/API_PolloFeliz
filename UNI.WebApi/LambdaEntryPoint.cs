fe8f30cd4f1574f5ee59b91865fb816e0236fb25
Eddy <caleroeddy99@gmail.com>
2025-10-26 21:06:21 -0600

    Creando estructura del proyecto

---
+++ UNI.WebApi/LambdaEntryPoint.cs
@@ -0,0 +1,19 @@
+namespace UNI.WebApi;
+
+
+public class LambdaEntryPoint :
+
+   
+    Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+{
+    protected override void Init(IWebHostBuilder builder)
+    {
+        builder
+            .UseStartup<Startup>();
+    }
+
+  
+    protected override void Init(IHostBuilder builder)
+    {
+    }
+}
